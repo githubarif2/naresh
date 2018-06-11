93801cb52759e93288abb8504ae8118674d80a8e
Asia <Asia@Asia-PC>
2018-06-11 14:17:07 +0530

    non conflict

--- Simple/src/com/nt/test/BA.java
+++ Simple/src/com/nt/test/BA.java
@@ -4,8 +4,8 @@ public class BA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stubs
-		System.out.println("BA.main()12");
-		System.err.println("naresh1");
+		System.out.println("BA.main()12");	
+
 
 	}
 
