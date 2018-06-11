764247ccc7f61840ac54fb19d96d5e487711dd17
Asia <Asia@Asia-PC>
2018-06-11 14:19:22 +0530

    dev non condr

--- Simple/src/com/nt/test/BA.java
+++ Simple/src/com/nt/test/BA.java
@@ -5,7 +5,6 @@ public class BA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stubs
 		System.out.println("BA.main()");
-		System.err.println("naresh");
 System.out.println("");
 	}
 
